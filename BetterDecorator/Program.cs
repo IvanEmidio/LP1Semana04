8175a58e7d8b6866ad835240e21ff1607cd0e6a0
IvanEmidio <ivan.emidio@live.com.pt>
2024-03-13 12:53:15 +0000

    Better Decorator Ex1

---
+++ BetterDecorator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace BetterDecorator
+{
+    class Program
+    {
+        private static string Decor(string s, char dec, int num)
+        {
+            string resumo = ""; 
+            for (int i = 0; i<num; i++)
+            {
+                resumo += $"{dec}";
+            }
+            resumo += $"{s}";
+
+            for (int i = 0; i<num; i++)
+            {
+                resumo += $"{dec}";
+            }
+            return resumo;
+        }
+        static void Main(string[] args)
+        
+        {
+            Console.WriteLine(Decor("Lost Forever", '.', 3 ));
+            
+        }
+    }
+}
