75809eebfef586c5c913d370a977c359180794a9
Ivan <ivan.emidio@live.com.pt>
2024-03-14 19:26:43 +0000

    Ex2 Comentarios

--- BetterDecorator/Program.cs
+++ BetterDecorator/Program.cs
@@ -2,8 +2,26 @@
 
 namespace BetterDecorator
 {
+    /// <summary>
+    /// Programa que recebe 3 parâmetros e imprime-os de acordo com a função 
+    /// Decor, imprime um char no ínicio e no fim um número de int vezes com uma
+    /// string no meio.
+    /// Porém caso não receba 3 parâmetros, imprime outra coisa
+    /// </summary>
     class Program
     {
+        
+        /// <summary>
+        /// Função Decor e recebe uma string, um char e um int e envia o char
+        /// repetido um número int de vezes no fim e no início e no meio mete a
+        /// string
+        /// </summary>
+        /// <param name="s">String recebida para imprimir no meio da
+        /// frase</param>
+        /// <param name="c">Char recebido para imprimir um número de vezes no
+        /// início e fim da string</param>
+        /// <param name="i">Int recebido para repetir o char</param>
+        /// <returns>Uma string</returns>
         private static string Decor(string s, char dec, int num)
         {
             string resumo = ""; 
@@ -19,6 +37,10 @@ namespace BetterDecorator
             }
             return resumo;
         }
+
+        /// <summary>
+        /// recebe uma string e imprime
+        /// </summary>
         static void Main(string[] args)
         
         {
